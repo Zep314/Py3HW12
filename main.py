f2cff84b21db9d87d50b3e6ba92afffdcc8b6546
Dmitry Mordanov <mordanov314@gmail.com>
2023-06-24 23:12:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Погружение в Python (семинары)
+# Урок 11. ООП. Особенности Python
+
+# Создайте класс Матрица. Добавьте методы для:
+# вывода на печать,
+# сравнения,
+# сложения,
+# *умножения матриц
+
+
+import my_utils5 as mu5
+
+if __name__ == '__main__':
+    # print('--== Тестирование класса MyString ==--')
+    # my_string = mu4.MyString('Привет!', 'Марк Лутц')
+    # print(f'{my_string=}')
+    # print(f'{my_string.author=}')
+    # print(f'{my_string.date_time=}')
+    # print(f'{my_string.lower()=}')
+
+    pass
